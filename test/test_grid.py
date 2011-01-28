2fd06e0c39204fa630574f12e042c68b58a748e5
Christophe Pradal <christophe.pradal@inria.fr>
2011-01-28 17:23:53 +0000

    fix the test
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk/pyratp@1203 0978ab36-7443-0410-8248-c2aa344a765a

--- test/test_grid.py
+++ test/test_grid.py
@@ -4,7 +4,8 @@ import alinea.pyratp.pyratp as r
 
 def test_read():
     fn = 'essaiRATP2/RATP/grille/grid3Da_2004.grd'
-    grid.read(fn)
+    g = grid.Grid()
+    g.read(fn)
 
     grid3d = r.grid3d
     assert (grid3d.njx, grid3d.njy ,grid3d.njz) == (19,20,18)
