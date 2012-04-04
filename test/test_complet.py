6012d865feefc27b4519cb04dbfc526edc4da42b
Christophe Pradal <christophe.pradal@inria.fr>
2012-04-04 10:00:25 +0000

    Add test
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk/pyratp@1916 0978ab36-7443-0410-8248-c2aa344a765a

---
+++ test/test_complet.py
@@ -0,0 +1,23 @@
+from openalea.core.alea import run
+from openalea.core.pkgmanager import PackageManager
+
+""" A unique PackageManager is created for all test of dataflow """
+
+pm = PackageManager()
+pm.init(verbose=False)
+
+def test_complet():
+    factory = pm['pyratp.demo']['complet2']
+    cn = factory.instantiate()
+    #for i in range(5):
+    cn.eval_as_expression(31)
+
+def test_can():
+    factory = pm['pyratp.demo']['RATP with Can Files']
+    cn = factory.instantiate()
+    cn.eval_as_expression(15)
+    result = cn.node(15).get_output(0)
+    
+    assert len(result) == 230
+    assert result.shape == 230,9
+    assert 199 < result.mean() < 200
