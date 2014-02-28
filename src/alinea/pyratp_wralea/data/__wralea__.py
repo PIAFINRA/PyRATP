29fd04384a117f483967273bb5869819256a355f
Boris Adam <Boris.Adam@clermont.inra.fr>
2014-02-28 13:47:33 +0000

    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk/pyratp@3557 0978ab36-7443-0410-8248-c2aa344a765a

--- src/alinea/pyratp_wralea/data/__wralea__.py
+++ src/alinea/pyratp_wralea/data/__wralea__.py
@@ -1,5 +1,5 @@
 
-# This file has been generated at Fri Jun 21 11:32:40 2013
+# This file has been generated at Thu Feb 20 15:07:25 2014
 
 from openalea.core import *
 
@@ -17,157 +17,80 @@ __institutes__ = ''
 __icon__ = ''
 
 
-__all__ = ['_111570800', '_71910320', '_71910576', '_71910736', '_71910512', '_71910416', '_71910352', '_71910800', '_71910480', '_71910864', '_71910448', '_71910608', '_71910704', '_71910544', '_71910256', '_71910288', '_71910768', '_71910672', '_71910384', '_71910640', '_71910832', '_71910224']
+__all__ = ['_70337392', '_70337424', '_70337520', '_70337360', '_70337648', '_130681104', '_70337456', '_70337488', '_70337616', '_70337552', '_70337584']
 
 
-_111570800 = DataFactory(name='vgxTestTriangles.vgx',
+_70337392 = DataFactory(name='aa2004pastroppetit.vgx',
                     description='',
                     editors=None,
                     includes=None,
                     )
 
 
-_71910320 = DataFactory(name='mtg_test.txt',
+_70337424 = DataFactory(name='mtg_test.txt',
                     description='MTG Apple Tree',
                     editors=None,
                     includes=None,
                     )
 
 
-_71910576 = DataFactory(name='grid3Da_2004.grd',
+_70337520 = DataFactory(name='vgxTestTriangles.vgx',
                     description='',
                     editors=None,
                     includes=None,
                     )
 
 
-_71910736 = DataFactory(name='grid3Da_2004_test.grd',
+_70337360 = DataFactory(name='mmeteo082050_1h.mto',
                     description='',
                     editors=None,
                     includes=None,
                     )
 
 
-_71910512 = DataFactory(name='grass_plt.can',
+_70337648 = DataFactory(name='mmeteo052000.mto',
                     description='',
                     editors=None,
                     includes=None,
                     )
 
 
-_71910416 = DataFactory(name='aa2004.vgx',
+_130681104 = DataFactory(name='matGridRATP_demo.mat',
                     description='',
                     editors=None,
                     includes=None,
                     )
 
 
-_71910352 = DataFactory(name='aa2004pastroppetit.vgx',
+_70337456 = DataFactory(name='PommierSuisse2004.veg',
                     description='',
                     editors=None,
                     includes=None,
                     )
 
 
-_71910800 = DataFactory(name='PommierSuisse2004.veg',
+_70337488 = DataFactory(name='grass_plt.can',
                     description='',
                     editors=None,
                     includes=None,
                     )
 
 
-_71910480 = DataFactory(name='mmeteo052000.mto',
+_70337616 = DataFactory(name='grid3Da_2004.grd',
                     description='',
                     editors=None,
                     includes=None,
                     )
 
 
-_71910864 = DataFactory(name='vegetationa_2004_test.vfn',
+_70337552 = DataFactory(name='skyvaultsoc.skv',
                     description='',
                     editors=None,
                     includes=None,
                     )
 
 
-_71910448 = DataFactory(name='digitalAD2004.dgt',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_71910608 = DataFactory(name='digitalAA2004.vgx',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_71910704 = DataFactory(name='vgxTest.vgx',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_71910544 = DataFactory(name='mmeteo082050.mto',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_71910256 = DataFactory(name='mmeteo082050_1h.mto',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_71910288 = DataFactory(name='aaa20041ligne.dgt',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_71910768 = DataFactory(name='grid3Da_grass_.grd',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_71910672 = DataFactory(name='aa2004petit.vgx',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_71910384 = DataFactory(name='grid3D_4_4_4.grd',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_71910640 = DataFactory(name='skyvaultsoc.skv',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_71910832 = DataFactory(name='MinePommier2004.veg',
-                    description='',
-                    editors=None,
-                    includes=None,
-                    )
-
-
-_71910224 = DataFactory(name='vegetationa_2004.vfn',
+_70337584 = DataFactory(name='vegetationa_2004.vfn',
                     description='',
                     editors=None,
                     includes=None,
