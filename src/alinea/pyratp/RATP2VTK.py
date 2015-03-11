b0bc7c79beb20ce62aee2b98e0185b28b0b3df6e
Boris Adam <Boris.Adam@clermont.inra.fr>
2015-03-11 12:29:24 +0000

    Encore un Bug corrige sur la fonction RATPVOXELS2VTK
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk/pyratp@5098 0978ab36-7443-0410-8248-c2aa344a765a

--- src/alinea/pyratp/RATP2VTK.py
+++ src/alinea/pyratp/RATP2VTK.py
@@ -85,6 +85,91 @@ def RATP2VTK(scene, variable,varname="Variable",nomfich="C:\tmpRATP\RATPOUT.vtk"
     # return outputs
     return   triangleColor
 
+
+def RATP2VTKNew(scene, variable,varname="Variable",nomfich="C:\tmpRATP\RATPOUT.vtk"):
+    '''    Display leaves colored by voxel values with Paraview for each entity
+           Scene is written in VTK Format as an unstructured grid
+           Inputs: ... a RATP variable = liste de float
+                   ... a scene plant GL composed of triangulated leaves
+           Outputs: ... a VTK file
+            T = all.Tesselator()
+            sce[0].apply(T)
+            T.result
+    '''
+
+    if len(variable)<len(scene):
+      variable=np.zeros(len(scene))
+
+
+    T = all.Tesselator()
+    VertexCoords=[]
+    TrianglesVertexIDs=[]
+    triangleColor = []
+
+    for k,i in enumerate(scene):
+         i.apply(T) #Applique Tesselator
+         TS = T.result
+         for vertex in TS.pointList:
+              VertexCoords.append([vertex[0],vertex[1],vertex[2]])
+
+         ShapeNumTri = len(TS.pointList) #nbr points dans  TriangleSet
+         for tri in TS.indexList:
+              TrianglesVertexIDs.append([tri[0]+k*ShapeNumTri,tri[1]+k*ShapeNumTri,tri[2]+k*ShapeNumTri])
+              triangleColor.append(variable[k])
+
+    numvertex = len(VertexCoords)
+    numTriangles = len(TrianglesVertexIDs)
+    # write the node code here.
+        # Write the output file following VTK file format for 3D view with Paraview
+        # Works only with triangles P1 i.e. defined with 3 points
+        #... Input:
+            #... Triangles - self attribute
+            #... Variable to be plotted - var
+            #... Corresponding variable name - varname
+        #... Output:
+            #... a VTK file - filename
+##    print nomfich
+    f=open(nomfich,'w')
+    # Set the header
+    f.write('# vtk DataFile Version 3.0\n')
+    f.write('vtk output\n')
+    f.write('ASCII\n')
+    f.write('DATASET UNSTRUCTURED_GRID\n')
+    f.write('POINTS '+str(numvertex)+' float\n')
+
+    # Write vertex coordinates
+    for i in VertexCoords:
+     f.write(str(i[0])+' '+str(i[1])+' '+str(i[2])+'\n')
+    f.write('\n')
+
+    # Write elements connectivity
+    f.write('CELLS '+str(numTriangles)+' '+str(numTriangles*4)+'\n')
+    for i in  TrianglesVertexIDs:
+     f.write('3 '+str(i[0])+' '+str(i[1])+' '+str(i[2])+'\n')
+    f.write('\n')
+
+    # Write elements type i.e. 5 for triangles
+    f.write('CELL_TYPES '+str(numTriangles)+'\n')
+    for i in  TrianglesVertexIDs:
+        f.write('5\n')
+    f.write('\n')
+
+    # Write data for each triangle
+    f.write('CELL_DATA '+str(numTriangles)+'\n')
+
+    f.write('FIELD FieldData 1 \n');
+    f.write(varname +' 1 '+str(numTriangles)+' float\n')
+    for i in triangleColor:
+          f.write(str(i).strip('[]')+'\n')
+
+    f.write('\n')
+
+    f.close()
+
+    # return outputs
+    return   triangleColor
+
+
 def RATPVOXELS2VTK(grid, variable,varname="Variable",nomfich="C:\tmpRATP\RATPOUT.vtk"):
     '''    Display Voxels colored by variable with Paraview
            RATP Grid is written in VTK Format as a structured grid
@@ -154,8 +239,8 @@ def RATPVOXELS2VTK(grid, variable,varname="Variable",nomfich="C:\tmpRATP\RATPOUT
         for ii in range(grid.njx):
           k =grid.kxyz[ii,ij,ik] #Get the voxel id number
           if (k>0):              #If the voxel k gets some vegetation then
-           #print k
-           f.write(str(variable[0][k])+'\n')
+##           print k
+           f.write(str(variable[0][k-1])+'\n')
           else:
            f.write(str(-9999.0)+'\n')
 
