e8f4db33851d6f3fdf4a7bef22972a34b3622333
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2017-05-02 12:11:12 +0000

    add test for several color per shape
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk/pyratp@6094 0978ab36-7443-0410-8248-c2aa344a765a

--- test/test_interface/test_pgl_scene.py
+++ test/test_interface/test_pgl_scene.py
@@ -38,11 +38,19 @@ if pgls.pgl_imported:
         vertices = ((0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0, 1))
         s_mesh = {1: (vertices, (faces[1],)),
                   2: (vertices, [faces[i] for i in (0, 2)])}
+        # default color
         scene = pgls.from_scene_mesh(s_mesh)
         assert len(scene) == 2
         assert scene[0].geometry.colorList[0].green == 180
-
+        # one color per shape
         scene = pgls.from_scene_mesh(s_mesh,
                                      colors={1: (0, 0, 0), 2: (1, 1, 1)})
         assert scene[0].geometry.colorList[0].green == 0
         assert scene[1].geometry.colorList[0].green == 1
+        # one color per triangle
+        scene = pgls.from_scene_mesh(s_mesh,
+                                     colors={1: (0, 0, 0),
+                                             2: [(1, 1, 1), (2, 2, 2)]})
+        assert scene[0].geometry.colorList[0].green == 0
+        assert scene[1].geometry.colorList[0].green == 1
+        assert scene[1].geometry.colorList[1].green == 2
